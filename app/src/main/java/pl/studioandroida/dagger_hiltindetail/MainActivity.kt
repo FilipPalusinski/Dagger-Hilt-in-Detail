4ae78c2fab70a6e84873ceff582a077cb7e1560f
Filip Palusiński <palusinskifilip@gmail.com>
2021-09-04 17:17:41 +0200

    Initial commit

---
+++ app/src/main/java/pl/studioandroida/dagger_hiltindetail/MainActivity.kt
@@ -0,0 +1,17 @@
+package pl.studioandroida.dagger_hiltindetail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+import javax.inject.Named
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/pl/studioandroida/dagger_hiltindetail/MyApplication.kt
@@ -0,0 +1,8 @@
+package pl.studioandroida.dagger_hiltindetail
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : Application() {
+}
